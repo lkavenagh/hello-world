76d356c42fcfe5845520f5343fe7e959802409a6
lkavenagh <lkavenagh@gmail.com>
2013-11-11 17:34:38 -0500

    Updated weather layout and weather.js functionality

--- weather.js
+++ weather.js
@@ -1,39 +1,106 @@
 jQuery(document).ready(function($) {
-	var location;
+	var city;
 	var state;
 	var country;
 	var requesturl;
+	var conditionsurl = 'NA';
 	var apikey = "91f86ce5a898ddbc";
 	
 	$( "#weatherautotemp" ).click(function() {
 		$( this ).fadeTo('medium', 0);
-		showTemp("http://api.wunderground.com/api/" + apikey + "/geolookup/q/autoip.json", this.attributes.item(0).nodeValue);
+		$( this ).next(".forecastdiv").fadeTo('medium', 0);
+		showWeatherReport("http://api.wunderground.com/api/" + apikey + "/geolookup/q/autoip.json", this.attributes.item(0).nodeValue);
 	});
 	
 	$( "#weatherziptemp" ).click(function() {
 		var zip=prompt("Enter a ZIP code","10001");
 		$( this ).fadeTo('medium', 0.01);
-		showTemp("http://api.wunderground.com/api/" + apikey + "/geolookup/q/" + zip + ".json", this.attributes.item(0).nodeValue);
+		$( this ).next(".forecastdiv").fadeTo('medium', 0);
+		showWeatherReport("http://api.wunderground.com/api/" + apikey + "/geolookup/q/" + zip + ".json", this.attributes.item(0).nodeValue);
 	});
 	
 	$( "#weathercitytemp" ).click(function() {
 		var input=prompt("Enter <city>,<country> or <city>,<statecode> for USA","Paris,France");
 		var inputSplit = input.split(',');
 		$( this ).fadeTo('medium', 0.01);
-		showTemp("http://api.wunderground.com/api/" + apikey + "/geolookup/q/" + inputSplit[1] + "/" + inputSplit[0] + ".json", this.attributes.item(0).nodeValue);
+		$( this ).next(".forecastdiv").fadeTo('medium', 0);
+		showWeatherReport("http://api.wunderground.com/api/" + apikey + "/geolookup/q/" + inputSplit[1] + "/" + inputSplit[0] + ".json", this.attributes.item(0).nodeValue);
 	});
 	
 	$( "#weatherbktemp" ).click(function() {
 		$( this ).fadeTo('medium', 0.01);
-		$( "#weatherbkfc" ).fadeTo('medium', 0.01);
-		showTemp("http://api.wunderground.com/api/" + apikey + "/geolookup/q/11222.json", this.attributes.item(0).nodeValue);
-		showTemp("http://api.wunderground.com/api/" + apikey + "/geolookup/q/11222.json", 'weatherbkfc');
+		$( this ).next(".forecastdiv").fadeTo('medium', 0);
+		showWeatherReport("http://api.wunderground.com/api/" + apikey + "/geolookup/q/11222.json", this.attributes.item(0).nodeValue);
 	});
 	
-	function showTemp($thisurl, $thishandle) {
-		if ($thishandle == "weatherbkfc") {
+	function showWeatherReport($thisurl, $thishandle) {
+		
+		var promise = getUrlForLocation($thisurl);
+		promise.done(function() {
+			showConditions($thishandle);
+			showForecast($thishandle);
+		});
+	}
+	
+	function getUrlForLocation($thisurl) {
+		var dfd = new $.Deferred();
+		$.ajax({
+			url : $thisurl,
+			dataType : "jsonp",
+			success : function(parsed_json) {
+				city = parsed_json['location']['city'];
+				state = parsed_json['location']['state'];
+				country = parsed_json['location']['country_name'];
+				if (country!="USA") {
+					conditionsurl = "http://api.wunderground.com/api/" + apikey + "/conditions/q/" + country.replace(/\s/gi, "_") + "/" + city.replace(/\s/gi, "_") + ".json";
+				} else {
+					conditionsurl = "http://api.wunderground.com/api/" + apikey + "/conditions/q/" + state.replace(/\s/gi, "_") + "/" + city.replace(/\s/gi, "_") + ".json";
+				}
+				dfd.resolve();
+			}
+		});
+		return dfd.promise();
+	};
+	
+	function showConditions($thishandle) {
+		$.ajax({
+			url : conditionsurl,
+			dataType : "jsonp",
+			success : function(parsed_json) {
+				temp_f = parsed_json['current_observation']['temp_f'];
+				temp_c = parsed_json['current_observation']['temp_c'];
+				humidity = parsed_json['current_observation']['relative_humidity'];
+				weather = parsed_json['current_observation']['weather'];
+				if ($thishandle == "weathercitytemp") {
+					document.getElementById($thishandle).innerHTML = "<p><b>Current Conditions (click to refresh)</b><br>" + city + ", " + country + ": " + temp_f + "F / " + temp_c + "C<br>Humidity: " + humidity + ", " + weather + "</p>";
+				} else {
+					document.getElementById($thishandle).innerHTML = "<p><b>Current Conditions (click to refresh)</b><br>" + city + ", " + state + " (" + country + "): " + temp_f + "F / " + temp_c + "C<br>Humidity: " + humidity + ", " + weather + "</p>";
+				}
+				$( "#" + $thishandle ).fadeTo('medium', 1);
+			},
+			error : function() {
+				document.getElementById($thishandle).innerHTML = "<p>Could not load weather! - click to refresh</p>";
+				$( "#" + $thishandle ).fadeTo('medium', 1);
+			}
+		});
+	};
+	
+	function showForecast($thishandle) {
+		$.when(
 			$.ajax({
-				url : "http://api.wunderground.com/api/" + apikey + "/forecast10day/q/NY/Brooklyn.json",
+				url : "http://api.wunderground.com/api/" + apikey + "/forecast10day/q/" + state + "/" + city + ".json",
+				dataType : "jsonp",
+				success : function(parsed_json) {
+					if(parsed_json["forecast"]==undefined) {
+						requesturl = "http://api.wunderground.com/api/" + apikey + "/forecast10day/q/" + country + "/" + city + ".json";
+					} else {
+						requesturl = "http://api.wunderground.com/api/" + apikey + "/forecast10day/q/" + state + "/" + city + ".json";
+					}
+				}
+			})
+		).then( function() {
+			$.ajax({
+				url : requesturl,
 				dataType : "jsonp",
 				success : function(parsed_json) {
 					var tmp_str = "";
@@ -45,52 +112,8 @@ jQuery(document).ready(function($) {
 						highF = parsed_json['forecast']['simpleforecast']['forecastday'][i]['high']['fahrenheit'];
 						tmp_str = tmp_str + "<br>" + weekday + ", " + month + " " + day + ": " + highF + "F / " + highC + "C";
 					}
-					document.getElementById($thishandle).innerHTML = "<p><b>Forecast</b>" + tmp_str + "</p>";
-				},
-				error : function() {
-					document.getElementById($thishandle).innerHTML = "<p>Could not load weather! - click to refresh</p>";
-					$( "#" + $thishandle ).fadeTo('medium', 1);
-				}
-			});
-			return;
-		}
-		$.when(
-			$.ajax({
-				url : $thisurl,
-				dataType : "jsonp",
-				success : function(parsed_json) {
-					city = parsed_json['location']['city'];
-					state = parsed_json['location']['state'];
-					country = parsed_json['location']['country_name'];
-					if ($thishandle == "weathercitytemp") {
-						if (country=="USA") {
-							requesturl = "http://api.wunderground.com/api/" + apikey + "/conditions/q/" + state.replace(/\s/gi, "_") + "/" + city.replace(/\s/gi, "_") + ".json";
-						} else {
-							requesturl = "http://api.wunderground.com/api/" + apikey + "/conditions/q/" + country.replace(/\s/gi, "_") + "/" + city.replace(/\s/gi, "_") + ".json";
-						}
-					} else {
-						requesturl = parsed_json['location']['requesturl'];
-						requesturl = jQuery.trim(requesturl).substring(0,requesturl.length-4);
-						requesturl = "http://api.wunderground.com/api/" + apikey + "/conditions/q/" + requesturl + "json";
-					}
-				}
-			})
-		).then( function(){
-			$.ajax({
-				url : requesturl,
-				dataType : "jsonp",
-				success : function(parsed_json) {
-					temp_f = parsed_json['current_observation']['temp_f'];
-					temp_c = parsed_json['current_observation']['temp_c'];
-					humidity = parsed_json['current_observation']['relative_humidity'];
-					weather = parsed_json['current_observation']['weather'];
-					if ($thishandle == "weathercitytemp") {
-						document.getElementById($thishandle).innerHTML = "<p><b>Current Conditions (click to refresh)</b><br>" + city + ", " + country + ": " + temp_f + "F / " + temp_c + "C<br>Humidity: " + humidity + ", " + weather + "</p>";
-					} else {
-						document.getElementById($thishandle).innerHTML = "<p><b>Current Conditions (click to refresh)</b><br>" + city + ", " + state + " (" + country + "): " + temp_f + "F / " + temp_c + "C<br>Humidity: " + humidity + ", " + weather + "</p>";
-					}
-					$( "#" + $thishandle ).fadeTo('medium', 1);
-					$( "#weatherbkfc" ).fadeTo('medium', 1);
+					$("#" + $thishandle).next(".forecastdiv").html("<p><b>Forecast for " + city + "</b>" + tmp_str + "</p>");
+					$( "#" + $thishandle ).next(".forecastdiv").fadeTo('medium', 1);
 				},
 				error : function() {
 					document.getElementById($thishandle).innerHTML = "<p>Could not load weather! - click to refresh</p>";
@@ -98,5 +121,5 @@ jQuery(document).ready(function($) {
 				}
 			});
 		});
-	}
+	};
 });
