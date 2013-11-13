976c1610d9cd46b5ef8cc1efc792421b8b172ac2
lkavenagh <lkavenagh@gmail.com>
2013-11-13 15:24:27 -0500

    Changed bitcoin api to bitcoinaverage.com

--- scripts/bitcoin.js
+++ scripts/bitcoin.js
@@ -1,10 +1,34 @@
 jQuery(document).ready(function($) {
 	var apikey = 'dea3b122de16ef0e377a94ee58890a3f728b92b119cd0b7609a2cb6399e02891';
-	var apiurl = 'https://coinbase.com/api/v1/';
+	var apiurl = 'https://api.bitcoinaverage.com/';
 	
 	$.ajax({
-		url : apiurl + 'prices/spot_rate',
-		success : function(data) { alert(data); },
-		error : function(message, a, b) { alert(message.status + ' ' + b); }
+		url : apiurl+'ticker/USD',
+		beforeSend : function() { console.log('Sending AJAX'); },
+		dataType : 'json',
+		success : function(data) {
+			$('#tradeprice').html(data['last']);
+			$('#askprice').html(data['ask']);
+			$('#bidprice').html(data['bid']);
+		},
+		error : function(a,b,c) {
+			alert(c);
+		}
 	});
+	
+	setInterval( function() {
+		$.ajax({
+			url : apiurl+'ticker/USD',
+			beforeSend : function() { console.log('Sending AJAX'); },
+			dataType : 'json',
+			success : function(data) {
+				$('#tradeprice').html(data['last']);
+				$('#askprice').html(data['ask']);
+				$('#bidprice').html(data['bid']);
+			},
+			error : function(a,b,c) {
+				alert(c);
+			}
+		});
+	}, 10000);
 });
