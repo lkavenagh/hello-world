aa9ad64e36a025b6610f68a24b9ebc66ab120b07
Luke <lkavenagh@gmail.com>
2013-11-13 23:06:00 -0500

    .

--- scripts/bitcoin.js
+++ scripts/bitcoin.js
@@ -1,34 +1,42 @@
 jQuery(document).ready(function($) {
 	var apikey = 'dea3b122de16ef0e377a94ee58890a3f728b92b119cd0b7609a2cb6399e02891';
 	var apiurl = 'https://api.bitcoinaverage.com/';
+	var currency = 'USD';
+	var prices = [];
 	
-	$.ajax({
-		url : apiurl+'ticker/USD',
-		beforeSend : function() { console.log('Sending AJAX'); },
-		dataType : 'json',
-		success : function(data) {
-			$('#tradeprice').html(data['last']);
-			$('#askprice').html(data['ask']);
-			$('#bidprice').html(data['bid']);
-		},
-		error : function(a,b,c) {
-			alert(c);
-		}
+	$.when(getCurrentPrices()).done(function() {
+		$('#tradeprice').html('Trade price: ' + prices[0]);
+		$('#bidprice').html('Bid price: ' + prices[1]);
+		$('#askprice').html('Ask price: ' + prices[2]);
+		$('#dailyavg').html('24 hour average: ' + prices[3]);
+		$('#timestamp').html('Last data received: ' + prices[4]);
 	});
 	
 	setInterval( function() {
-		$.ajax({
-			url : apiurl+'ticker/USD',
-			beforeSend : function() { console.log('Sending AJAX'); },
+		$.when(getCurrentPrices()).done(function() {
+		$('#tradeprice').html('Trade price: ' + prices[0]);
+		$('#bidprice').html('Bid price: ' + prices[1]);
+		$('#askprice').html('Ask price: ' + prices[2]);
+		$('#dailyavg').html('24 hour average: ' + prices[3]);
+		$('#timestamp').html('Last data received: ' + prices[4]);})
+	}, 10000);
+	
+	function getCurrentPrices() {
+		return $.ajax({
+			url : apiurl+'ticker/' + currency,
 			dataType : 'json',
 			success : function(data) {
-				$('#tradeprice').html(data['last']);
-				$('#askprice').html(data['ask']);
-				$('#bidprice').html(data['bid']);
+				prices[0] = data['last'];
+				prices[1] = data['bid'];
+				prices[2] = data['ask'];
+				prices[3] = data['24h_avg'];
+				var date = new Date(data['timestamp'].substring(0, data['timestamp'].length-5) + ' UTC');
+				
+				prices[4] = date.getFullYear() + '-' + ('0' + date.getMonth()).slice(-2) + '-' + ('0' + date.getDate()).slice(-2) + ', ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2);
 			},
-			error : function(a,b,c) {
+			error : function(message,b,c) {
 				alert(c);
 			}
 		});
-	}, 10000);
+	}
 });
