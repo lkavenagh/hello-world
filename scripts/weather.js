c927ed3f8490a59332e7c6d5e9cc793062efff70
lkavenagh <lkavenagh@gmail.com>
2013-11-14 10:53:54 -0500

    Weather api more efficient

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,24 +2,11 @@ jQuery(document).ready(function($) {
 	var city;
 	var state;
 	var country;
+	var querydata;
 	var requesturl;
 	var conditionsurl = 'NA';
 	var apikey = "818a91d82fc2c53d";
 	var tempform = "C";
-	
-	jQuery('body').ajaxStart(function(){
-	    alert( "ajaxStart" );
-	  }).ajaxStop(function(){
-	    alert( "ajaxStop" );
-	  }).ajaxSend(function(){
-	    alert( "ajaxSend" );
-	  }).ajaxComplete(function(){
-	    alert( "ajaxComplete" );
-	  }).ajaxError(function(){
-	    alert( "ajaxError" );
-	  }).ajaxSuccess(function(){
-	    alert( "ajaxSuccess" );
-	  });
 	
 	$("#tempformF").hide(0, function(){});
 	
@@ -67,122 +54,94 @@ jQuery(document).ready(function($) {
 	$( "#weatherbktemp" ).click(function() {
 		$( this ).fadeTo('medium', 0.01);
 		$( this ).next(".forecastdiv").fadeTo('medium', 0);
-		showWeatherReport("http://api.wunderground.com/api/" + apikey + "/geolookup/q/11222.json", this.attributes.item(0).nodeValue);
+		showWeatherReport("http://api.wunderground.com/api/" + apikey + "/geolookup/q/11222.json", this.attributes.item(0).nodeValue, '2013-11-28');
 	});
 	
 	function showWeatherReport($thisurl, $thishandle, specdate) {
-		var promise = getUrlForLocation($thisurl);
-
-		if ($thishandle=="weatherbktemp") {
-			promise.done(function() {
-				showConditions($thishandle);
-				showConditionsDate($thishandle, "2013-11-28");
+		$.when(getUrlForLocation($thisurl))
+		.then( function() {
+			$.when(showConditions($thishandle))
+			.then(function() { 
+				if (specdate==undefined) {
+					showForecast($thishandle);
+				} else {
+					showConditionsDate($thishandle, specdate);
+				}
 			});
-		} else {
-			promise.done(function() {
-				showConditions($thishandle);
-				showForecast($thishandle);
-			});
-		};
+		});
 	}
 	
 	function getUrlForLocation($thisurl) {
-		var dfd = new $.Deferred();
-		$.ajax({
+		return $.ajax({
 			url : $thisurl,
 			dataType : "jsonp",
-			beforeSend : function() { alert("getUrlForLocation.beforeSend"); },
 			success : function(parsed_json) {
 				city = parsed_json['location']['city'];
 				state = parsed_json['location']['state'];
 				country = parsed_json['location']['country_name'];
 				if (country!="USA") {
-					conditionsurl = "http://api.wunderground.com/api/" + apikey + "/conditions/q/" + country.replace(/\s/gi, "_") + "/" + city.replace(/\s/gi, "_") + ".json";
+					conditionsurl = "http://api.wunderground.com/api/" + apikey + "/conditions/forecast10day/q/" + country.replace(/\s/gi, "_") + "/" + city.replace(/\s/gi, "_") + ".json";
 				} else {
-					conditionsurl = "http://api.wunderground.com/api/" + apikey + "/conditions/q/" + state.replace(/\s/gi, "_") + "/" + city.replace(/\s/gi, "_") + ".json";
+					conditionsurl = "http://api.wunderground.com/api/" + apikey + "/conditions/forecast10day/q/" + state.replace(/\s/gi, "_") + "/" + city.replace(/\s/gi, "_") + ".json";
 				}
-				dfd.resolve();
 			},
 			error: function() {
-				document.getElementById($thishandle).innerHTML = "<p>Could not load weather! - click to refresh</p>";
+				$('#' + $thishandle).html("<p>Could not load weather! - click to refresh</p>");
 				$( "#" + $thishandle ).fadeTo('medium', 1);
 			}
 		});
-		return dfd.promise();
 	};
 	
 	function showConditions($thishandle) {
-		$.ajax({
+		return $.ajax({
 			url : conditionsurl,
 			dataType : "jsonp",
-			beforeSend : function() { alert("showConditions.beforeSend"); },
 			success : function(parsed_json) {
 				if (tempform=="C") {
-					temp = parsed_json['current_observation']['temp_c'];
+					querydata = parsed_json;
+					temp = querydata['current_observation']['temp_c'];
 				} else {
-					temp = parsed_json['current_observation']['temp_f'];
+					querydata = parsed_json;
+					temp = querydata['current_observation']['temp_f'];
 				}
 				humidity = parsed_json['current_observation']['relative_humidity'];
 				weather = parsed_json['current_observation']['weather'];
 				if ($thishandle == "weathercitytemp") {
-					document.getElementById($thishandle).innerHTML = "<p><b>Current Conditions (click to refresh)</b><br>" + city + ", " + country + ": " + temp + tempform + "<br>Humidity: " + humidity + ", " + weather + "</p>";
+					$('#' + $thishandle).html("<p><b>Current Conditions (click to refresh)</b><br>" + city + ", " + country + ": " + temp + tempform + "<br>Humidity: " + humidity + ", " + weather + "</p>");
 				} else {
-					document.getElementById($thishandle).innerHTML = "<p><b>Current Conditions (click to refresh)</b><br>" + city + ", " + state + " (" + country + "): " + temp + tempform + "<br>Humidity: " + humidity + ", " + weather + "</p>";
+					$('#' + $thishandle).html("<p><b>Current Conditions (click to refresh)</b><br>" + city + ", " + state + " (" + country + "): " + temp + tempform + "<br>Humidity: " + humidity + ", " + weather + "</p>");
 				}
 				$( "#" + $thishandle ).fadeTo('medium', 1);
 			},
 			error : function() {
-				document.getElementById($thishandle).innerHTML = "<p>Could not load weather! - click to refresh</p>";
+				$('#' + $thishandle).html("<p>Could not load weather! - click to refresh</p>");
 				$( "#" + $thishandle ).fadeTo('medium', 1);
 			}
 		});
 	};
 	
 	function showForecast($thishandle) {
-		document.getElementById($thishandle).innerHTML = "<p>Loading forecast...</p>";
-		$.when(
-			$.ajax({
-				url : "http://api.wunderground.com/api/" + apikey + "/forecast10day/q/" + state + "/" + city + ".json",
-				dataType : "jsonp",
-				beforeSend : function() { alert("showForecast1.beforeSend"); },
-				success : function(parsed_json) {
-					if(parsed_json["forecast"]==undefined) {
-						requesturl = "http://api.wunderground.com/api/" + apikey + "/forecast10day/q/" + country + "/" + city + ".json";
-					} else {
-						requesturl = "http://api.wunderground.com/api/" + apikey + "/forecast10day/q/" + state + "/" + city + ".json";
-					}
-				}
-			})
-		).then( function() {
-			$.ajax({
-				url : requesturl,
-				dataType : "jsonp",
-				beforeSend : function() { alert("showForecast2.beforeSend"); },
-				success : function(parsed_json) {
-					var tmp_str = "";
-					data = parsed_json['forecast']['simpleforecast']['forecastday'];
-					$.each(data, function(k,v) {
-						month = v['date']['monthname'];
-						day = v['date']['day'];
-						weekday = v['date']['weekday'];
-						if (tempform=="C") {
-							high = v['high']['celsius'];
-							low = v['low']['celsius'];
-						} else {
-							high = v['high']['fahrenheit'];
-							low = v['low']['fahrenheit'];
-						}
-						tmp_str = tmp_str + "<br>" + weekday + ", " + month + " " + day + ": " + low + " - " + high + tempform;
-					});
-					$("#" + $thishandle).next(".forecastdiv").html("<p><b>Forecast for " + city + "</b>" + tmp_str + "</p>");
-					$( "#" + $thishandle ).next(".forecastdiv").fadeTo('medium', 1);
-				},
-				error : function() {
-					document.getElementById($thishandle).innerHTML = "<p>Could not load weather! - click to refresh</p>";
-					$( "#" + $thishandle ).fadeTo('medium', 1);
-				}
-			});
+		$('#'+$thishandle).next(".forecastdiv").innerHTML = "<p>Loading forecast...</p>";
+
+		var tmp_str = "";
+		console.log(querydata);
+		data = querydata['forecast']['simpleforecast']['forecastday'];
+		$.each(data, function(k,v) {
+			month = v['date']['monthname'];
+			day = v['date']['day'];
+			weekday = v['date']['weekday'];
+			if (tempform=="C") {
+				high = v['high']['celsius'];
+				low = v['low']['celsius'];
+			} else {
+				high = v['high']['fahrenheit'];
+				low = v['low']['fahrenheit'];
+			}
+			tmp_str = tmp_str + "<br>" + weekday + ", " + month + " " + day + ": " + low + " - " + high + tempform;
 		});
+		$("#" + $thishandle).next(".forecastdiv").html("<p><b>Forecast for " + city + "</b>" + tmp_str + "</p>");
+		$( "#" + $thishandle ).next(".forecastdiv").fadeTo('medium', 1);
+			
 	};
 	
 	function showConditionsDate($thishandle, specdate) {
@@ -190,56 +149,31 @@ jQuery(document).ready(function($) {
 		var specmonth = specdate[5]+specdate[6];
 		var specday = specdate[8]+specdate[9];
 		var found = false;
-		$.when(
-			$.ajax({
-				url : "http://api.wunderground.com/api/" + apikey + "/forecast10day/q/" + state + "/" + city + ".json",
-				dataType : "jsonp",
-				beforeSend : function() { alert("showConditionsDate1.beforeSend"); },
-				success : function(parsed_json) {
-					if(parsed_json["forecast"]==undefined) {
-						requesturl = "http://api.wunderground.com/api/" + apikey + "/forecast10day/q/" + country + "/" + city + ".json";
-					} else {
-						requesturl = "http://api.wunderground.com/api/" + apikey + "/forecast10day/q/" + state + "/" + city + ".json";
-					}
+		
+		var tmp_str = "";
+		data = querydata['forecast']['simpleforecast']['forecastday'];
+		$.each(data, function(k,v) {
+			year = v['date']['year'];
+			month = v['date']['month'];
+			day = v['date']['day'];
+			if (year==specyear && month==specmonth && day==specday) {
+				found = true;
+				if (tempform=="C") {
+					high = v['high']['celsius'];
+					low = v['low']['celsius'];
+				} else {
+					high = v['high']['fahrenheit'];
+					low = v['low']['fahrenheit'];
 				}
-			})
-		).then( function() {
-			$.ajax({
-				url : requesturl,
-				dataType : "jsonp",
-				beforeSend : function() { alert("showConditionsDate2.beforeSend"); },
-				success : function(parsed_json) {
-					var tmp_str = "";
-					data = parsed_json['forecast']['simpleforecast']['forecastday'];
-					$.each(data, function(k,v) {
-						year = v['date']['year'];
-						month = v['date']['month'];
-						day = v['date']['day'];
-						if (year==specyear && month==specmonth && day==specday) {
-							found = true;
-							if (tempform=="C") {
-								high = v['high']['celsius'];
-								low = v['low']['celsius'];
-							} else {
-								high = v['high']['fahrenheit'];
-								low = v['low']['fahrenheit'];
-							}
-							tmp_str = tmp_str + "<br>" + weekday + ", " + month + " " + day + ": " + low + " - " + high + tempform;
-						}
-					});
-					if (found) {
-						$("#" + $thishandle).next(".forecastdiv").html("<p><b>Forecast for " + city + "</b>" + tmp_str + "</p>");
-						$( "#" + $thishandle ).next(".forecastdiv").fadeTo('medium', 1);
-					} else {
-						$("#" + $thishandle).next(".forecastdiv").html("<p><b>Forecast for " + city + "</b><br>" + specyear + "-" + specmonth + "-" + specday + " Not available!</p>");
-						$( "#" + $thishandle ).next(".forecastdiv").fadeTo('medium', 1);
-					}
-				},
-				error : function() {
-					document.getElementById($thishandle).innerHTML = "<p>Could not load weather! - click to refresh</p>";
-					$( "#" + $thishandle ).fadeTo('medium', 1);
-				}
-			});
+				tmp_str = tmp_str + "<br>" + weekday + ", " + month + " " + day + ": " + low + " - " + high + tempform;
+			}
 		});
+		if (found) {
+			$("#" + $thishandle).next(".forecastdiv").html("<p><b>Forecast for " + city + "</b>" + tmp_str + "</p>");
+			$( "#" + $thishandle ).next(".forecastdiv").fadeTo('medium', 1);
+		} else {
+			$("#" + $thishandle).next(".forecastdiv").html("<p><b>Forecast for " + city + "</b><br>" + specyear + "-" + specmonth + "-" + specday + " Not available!</p>");
+			$( "#" + $thishandle ).next(".forecastdiv").fadeTo('medium', 1);
+		}
 	};
 });
