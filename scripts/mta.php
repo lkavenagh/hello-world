e7513c674d6f65cf4ac9f3e491a74e65b6c94389
Luke <lkavenagh@gmail.com>
2013-11-29 13:03:20 -0500

    .

--- scripts/mta.php
+++ scripts/mta.php
@@ -1,3 +1,6 @@
 <?php
-echo file_get_contents($_POST['address']);
+$opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));
+$context = stream_context_create($opts);
+$header = file_get_contents($_POST['address'],false,$context);
+echo $header;
 ?>
