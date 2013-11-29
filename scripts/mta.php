7a2cb524375f37d94c97f5c48040d36435dde609
Luke <lkavenagh@gmail.com>
2013-11-29 13:06:18 -0500

    .

--- scripts/mta.php
+++ scripts/mta.php
@@ -1,5 +1,5 @@
 <?php
-$opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));
+$opts = array('http'=>array('header' => "User-Agent:Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.75 Safari/537.1\r\n"));
 $context = stream_context_create($opts);
 $header = file_get_contents($_POST['address'],false,$context);
 echo $header;
