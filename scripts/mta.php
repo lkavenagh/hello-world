fa8325ca1e6890905c39ee4054ecf9646d896ad7
Luke <lkavenagh@gmail.com>
2013-11-29 14:13:48 -0500

    .

--- scripts/mta.php
+++ scripts/mta.php
@@ -1,6 +1,69 @@
 <?php
-$opts = array('http'=>array('header' => "User-Agent:Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.75 Safari/537.1\r\n"));
-$context = stream_context_create($opts);
-$header = file_get_contents($_POST['address'],false,$context);
-echo $header;
+
+// Allowed hostname
+define ('HOSTNAME', 'http://localhost:80/');
+
+//returns the headers as an array
+function getHeaders()
+{
+    $headers = array();
+    foreach ($_SERVER as $k => $v)
+    {
+        if (substr($k, 0, 5) == "HTTP_")
+        {
+            $k = str_replace('_', ' ', substr($k, 5));
+            $k = str_replace(' ', '-', ucwords(strtolower($k)));
+            $headers[$k] = $v;
+        }
+    }
+    return $headers;
+} 
+
+// Get the REST call path from the AJAX application
+$path = $_REQUEST['path'];
+$url = HOSTNAME.$path;
+
+// Open the Curl session
+$session = curl_init($url);
+
+// If it's a POST, put the POST data in the body
+if ($_POST['path']) {
+ $postvars = '';
+ while ($element = current($_POST)) {
+  $postvars .= key($_POST).'='.$element.'&';
+  next($_POST);
+ }
+ $postvars = substr($postvars, 0, -1);  // removing trailing &
+ $postvars = str_replace('xml_version', 'xml version', $postvars);  // fix xml declaration bug?
+ $postvars = stripslashes($postvars);
+
+ curl_setopt ($session, CURLOPT_POST, true);
+ curl_setopt ($session, CURLOPT_POSTFIELDS, $postvars);
+}
+else {
+ //If it's a post, but not a form post (like a SOAP request)
+ if ($_SERVER['REQUEST_METHOD']==='POST') {
+  curl_setopt ($session, CURLOPT_POST, true);
+  curl_setopt ($session, CURLOPT_POSTFIELDS, $HTTP_RAW_POST_DATA);
+
+  $headers = getHeaders();
+  $header_array = Array( "Content-Type: text/xml", "SOAPAction: " . $headers['SOAPAction']);
+  curl_setopt ($session, CURLOPT_HTTPHEADER, $header_array);
+  curl_setopt ($session, CURLOPT_CUSTOMREQUEST, "POST");
+ }
+}
+
+// Don't return HTTP headers. Do return the contents of the call
+curl_setopt($session, CURLOPT_HEADER, false);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// Make the call
+$xml = curl_exec($session);
+
+// The web service returns XML. Set the Content-Type appropriately
+header("Content-Type: text/xml");
+
+echo $xml;
+curl_close($session);
+
 ?>
