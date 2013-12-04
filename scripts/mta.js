6b76fb85771076abda287ca7503cd60a9dd7cc21
lkavenagh <lkavenagh@gmail.com>
2013-12-04 12:41:09 -0500

    .

--- scripts/mta.js
+++ scripts/mta.js
@@ -1,16 +1,17 @@
 var proxyurl = './scripts/mta.php';
-var requesturl = 'http://web.mta.info/status/serviceStatus.txt'
+var requesturl = 'http://web.mta.info/status/serviceStatus.txt';
+var filepath = 'file:///R:/Documents/GitHub/kavenagh-website/serviceStatus.txt';
 var apikey = '01165445e3b0666d030436b417ef1aec';
 
 jQuery(document).ready(function($) {
 	$.ajax({
 		url : proxyurl,
-		dataType : 'text',
+		dataType : 'xml',
 		data: {
 			requrl: requesturl
 		},
 		success : function(data) {
-			console.log(data);
+			console.log(data['service']);
 		},
 		error : function(message, a, b) {
 			console.log(b);
