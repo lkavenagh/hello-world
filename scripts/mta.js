a0b3f4b5298c8151899fb2c47c36726a7f8b997b
kavenagh <kavenagh@martha.asmallorange.com>
2013-12-04 16:26:20 -0500

    .

--- scripts/mta.js
+++ scripts/mta.js
@@ -1,31 +1,48 @@
-var proxyurl = './scripts/mta.php';
+var proxyurl = './scripts/proxy.php';
 var requesturl = 'http://web.mta.info/status/serviceStatus.txt';
 var apikey = '01165445e3b0666d030436b417ef1aec';
 
 jQuery(document).ready(function($) {
-	$.ajax({
-		url : proxyurl,
-		dataType : 'text',
-		data: {
-			requrl: requesturl
-		},
-		success : function(data) {
-			xmlDoc = $.parseXML( data );
-			$('#mtafeed').html('<p>' + $(xmlDoc).find('service').find('timestamp').text() + '</p><br>');
-			$(xmlDoc).find('service').find('subway').find('line').each(function() {
-				name = $(this).find('name').text();
-				if($(this).find('status').text() != 'GOOD SERVICE') {
-					col = '<font color=\'red\'>';
-					statusStr = $(this).find('status').text() + ' - ' + $(this).find('text').text() + '</font>';
-				} else {
-					col = '<font color=\'green\'>';
-					statusStr = $(this).find('status').text() + '</font>';
-				}
-				$('#mtafeed').html($('#mtafeed').html() + '<tr><td>' + name + '</td><td>' + col + statusStr + '</td></tr>');
-			});
-		},
-		error : function(message, a, b) {
-			console.log(b);
-		}
+	$('#mtafeed').on('click', '.hover.mtastatus', function() {
+		$(this).next('.mtastatusdesc').slideToggle();
 	});
+
+	getMTAFeed();
+	setInterval( function() {
+		getMTAFeed();
+	}, 60000);
+	
+	function getMTAFeed() {
+		$.ajax({
+			url : proxyurl,
+			dataType : 'text',
+			data: {
+				requrl: requesturl
+			},
+			success : function(data) {
+				$('#mtafeed').html('');
+				xmlDoc = $.parseXML( data );
+				$(xmlDoc).find('service').find('subway').find('line').each(function() {
+					name = $(this).find('name').text();
+					if($(this).find('status').text() != 'GOOD SERVICE') {
+						statusStr = "<div class=\'hover mtastatus\'>" 
+							+ '<font color=\'red\'>' 
+							+ $(this).find('status').text() 
+							+ '</font><br></div><div class=\'mtastatusdesc\'> ' 
+							+ $(this).find('text').text() + '</div>';
+					} else {
+						statusStr = '<font color=\'green\'>' 
+							+ $(this).find('status').text() 
+							+ '</font>';
+					}
+					$('#mtafeed').append('<tr><td class=\'col1\'>' + name + '</td><td class = \'col2\'>' + statusStr + '</td></tr>');
+					$('#tablefooter').html('Last data received: ' + $(xmlDoc).find('service').find('timestamp').text());
+				});
+	
+			},
+			error : function(message, a, b) {
+				$('#mtafeed').append('<p> MTA API is down </p>');
+			}
+		});
+	}
 });
